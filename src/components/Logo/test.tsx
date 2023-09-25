99f3009d8f46b18854daa5718671cf3fedfc26dc
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2023-09-25 08:17:19 -0300

    :construction: feat: Logo created

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,6 +1,7 @@
-import { screen } from '@testing-library/react'
-import Logo from '.'
 import { renderWithTheme } from '../../utils/tests/helpers'
+import { render, screen } from '@testing-library/react'
+import 'jest-styled-components'
+import Logo from '.'
 
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
@@ -15,5 +16,30 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
     })
+  })
+
+  it('should render a normal logo whe size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a big logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    render(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
