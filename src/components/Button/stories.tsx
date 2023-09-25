7d601b258e93e00729de4700bc2cf4c37264a22a
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2023-09-25 16:14:18 -0300

    :construction: feat: Button Component

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material-rounded/AddShoppingCart'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta
+
+export const Default: StoryObj = {
+  args: {
+    children: 'Buy now'
+  }
+}
+
+export const withIcon: StoryObj = {
+  args: {
+    size: 'small',
+    children: 'Buy now',
+    icon: <AddShoppingCart />
+  }
+}
