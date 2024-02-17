e005014c1d93e5a264bcf22b21bcae9f99d8dc46
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-02-17 01:41:52 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    // render the component
+    const { container } = render(<Main />)
+
+    // search the element and verify if it exists
+    expect(
+      screen.getByRole('heading', {
+        name: /react avançado/i
+      })
+    ).toBeInTheDocument()
+
+    // snapshot
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    // render the component
+    const { container } = render(<Main />)
+
+    // verify if the background color is correct
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
+  })
+})
