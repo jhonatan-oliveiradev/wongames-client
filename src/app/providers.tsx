85da6aa12f07bb9e9175b1ca0baae53731111e1b
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-02-19 22:26:20 -0300

    feat: global theme styles

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import GlobalStyles from '@/styles/global'
+import theme from '@/styles/theme'
 import { PropsWithChildren } from 'react'
+import { ThemeProvider } from 'styled-components'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <>
-      <GlobalStyles />
-      {children}
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
     </>
   )
 }
