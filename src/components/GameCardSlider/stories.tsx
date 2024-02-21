d6ab3d9900f65df0a731ff3da6947fb4ff389cdd
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-02-20 21:38:33 -0300

    feat: GameCard, Slider and GameCardSlider Components

---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { GameCardProps } from '../GameCard'
+import GameCardSlider from '.'
+
+import items from './mock'
+
+export default {
+  title: 'GameCardSlider',
+  component: GameCardSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryObj<GameCardProps[]> = {
+  args: [],
+  render: (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <GameCardSlider items={args} {...args} />
+    </div>
+  )
+}
