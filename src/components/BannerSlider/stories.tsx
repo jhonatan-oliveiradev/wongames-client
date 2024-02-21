5ba047e29d06c4be662e1c61711195639eb9fdb5
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-02-20 21:42:09 -0300

    feat: BannerSlider Component

---
+++ src/components/BannerSlider/stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react'
+import BannerSlider, { BannerSliderProps } from '.'
+
+import items from './mock'
+
+export default {
+  title: 'BannerSlider',
+  component: BannerSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta<BannerSliderProps>
+
+export const Default: StoryObj<BannerSliderProps> = {
+  render: (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <BannerSlider {...args} />
+    </div>
+  )
+}
